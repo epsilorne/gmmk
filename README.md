e1f37b86ae2b451ac86a20cb8535f77f77c38482
Danh <32991333+epsilorne@users.noreply.github.com>
2025-09-12 17:09:01 +1000

    update: add generic send_report(), change program usage

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 Experimenting with reverse-engineering a GMMK Compact to control its LEDs.
 
-In the source code, you can toggle the LEDs on or off (assuming the other default options):
+Use with the `on` or `off` argument (with root permissions), e.g.
 
-```c
-// comment one or the other
-led_on(handle, buf);
-// led_off(handle, buf);
+```bash
+./driver on
+./driver off
 ```
 
 ## Build
 
 ```bash
-gcc src/driver.c -o driver -lhidapi-hidraw && sudo ./driver 
+gcc src/driver.c -o driver -lhidapi-hidraw
 ```
 
--- src/driver.c
+++ src/driver.c
@@ -31,118 +31,88 @@ int init(hid_device **handle) {
 }
 
 /**
- * Turn on the keyboard's LEDs by sending the right packets.
+ * Sends an output report containing the bytes 'to_send' (of size 'n'). 'buf' is
+ * a 64 byte buffer, while 'to_send' and 'n' are up to 63 bytes.
  */
-int led_on(hid_device *handle, unsigned char *buf) {
+int send_report(hid_device *handle, unsigned char *buf, unsigned char *to_send, size_t n) {
   int res;
-
-  // initial packet
-  buf[0] = 0x04;
-  buf[1] = 0x01;
-  buf[2] = 0x00;
-  buf[3] = 0x01;
-  res = hid_send_output_report(handle, buf, MAX_BUF);
-  if (res < 0) {
+  memset(buf, 0, MAX_BUF);
+  memcpy(buf, to_send, n);
+  if ((res = hid_send_output_report(handle, buf, MAX_BUF)) < 0) {
     fprintf(stderr, "failed to send output report\n");
     return -1;
   }
-  memset(buf, 0, MAX_BUF);
-
-  // packet to turn onled
-  buf[0] = 0x04;
-  buf[1] = 0x0c;
-  buf[2] = 0x00;
-  buf[3] = 0x06;
-  buf[4] = 0x01;
-  buf[5] = 0x04;
-  buf[6] = 0x00;
-  buf[7] = 0x00;
-  buf[8] = 0x01;
-  res = hid_send_output_report(handle, buf, MAX_BUF);
-  if (res < 0) {
-    fprintf(stderr, "failed to send output report\n");
-    return -1;
-  }
-  memset(buf, 0, MAX_BUF);
-
-  // end packet
-  buf[0] = 0x04;
-  buf[1] = 0x02;
-  buf[2] = 0x00;
-  buf[3] = 0x02;
-  res = hid_send_output_report(handle, buf, MAX_BUF);
-  if (res < 0) {
-    fprintf(stderr, "failed to send output report\n");
-    return -1;
-  }
-  memset(buf, 0, MAX_BUF);
-
-  return 0;
+  return res;
 }
 
 /**
  * Turn on the keyboard's LEDs by sending the right packets.
  */
-int led_off(hid_device *handle, unsigned char *buf) {
-  int res;
-
-  // initial packet
-  buf[0] = 0x04;
-  buf[1] = 0x01;
-  buf[2] = 0x00;
-  buf[3] = 0x01;
-  res = hid_send_output_report(handle, buf, MAX_BUF);
-  if (res < 0) {
+int led_on(hid_device *handle, unsigned char *buf) {
+  if (send_report(handle, buf, (unsigned char[]){0x04, 0x01, 0x00, 0x01}, 4) < 0) {
     fprintf(stderr, "failed to send output report\n");
     return -1;
   }
-  memset(buf, 0, MAX_BUF);
-
-  // packet to turn off led
-  buf[0] = 0x04;
-  buf[1] = 0x0b;
-  buf[2] = 0x00;
-  buf[3] = 0x06;
-  buf[4] = 0x01;
-  buf[5] = 0x04;
-  res = hid_send_output_report(handle, buf, MAX_BUF);
-  if (res < 0) {
+  if (send_report(handle, buf, (unsigned char[]){0x04, 0x0c, 0x00, 0x06, 0x01, 0x04, 0x00, 0x00, 0x01}, 9) < 0) {
     fprintf(stderr, "failed to send output report\n");
     return -1;
   }
-  memset(buf, 0, MAX_BUF);
-
-  // end packet
-  buf[0] = 0x04;
-  buf[1] = 0x02;
-  buf[2] = 0x00;
-  buf[3] = 0x02;
-  res = hid_send_output_report(handle, buf, MAX_BUF);
-  if (res < 0) {
+  if (send_report(handle, buf, (unsigned char[]){0x04, 0x02, 0x00, 0x02}, 4) < 0) {
     fprintf(stderr, "failed to send output report\n");
     return -1;
   }
-  memset(buf, 0, MAX_BUF);
+  return 0;
 
   return 0;
 }
 
+/**
+ * Turn off the keyboard's LEDs by sending the right packets.
+ */
+int led_off(hid_device *handle, unsigned char *buf) {
+  if (send_report(handle, buf, (unsigned char[]){0x04, 0x01, 0x00, 0x01}, 4) < 0) {
+    fprintf(stderr, "failed to send output report\n");
+    return -1;
+  }
+  if (send_report(handle, buf, (unsigned char[]){0x04, 0x0b, 0x00, 0x06, 0x01, 0x04}, 6) < 0) {
+    fprintf(stderr, "failed to send output report\n");
+    return -1;
+  }
+  if (send_report(handle, buf, (unsigned char[]){0x04, 0x02, 0x00, 0x02}, 4) < 0) {
+    fprintf(stderr, "failed to send output report\n");
+    return -1;
+  }
+  return 0;
+}
+
 int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "usage: driver <on/off>\n");
+    exit(EXIT_FAILURE);
+  }
+
   int res;
   unsigned char buf[64] = {0};
   hid_device *handle;
   
   res = hid_init();
 
-  if (init(&handle)) {
+  if (init(&handle) < 0) {
     fprintf(stderr, "could not init keyboard\n");
     hid_exit();
     exit(EXIT_FAILURE);
   }
 
-  // comment one or the other
-  led_on(handle, buf);
-  // led_off(handle, buf);
+  if (strcmp(argv[1], "on") == 0) {
+    if (led_on(handle, buf) < 0) {
+      fprintf(stderr, "failed to turn on led\n");
+    }
+  }
+  else if (strcmp(argv[1], "off") == 0) {
+    if (led_off(handle, buf) < 0) {
+      fprintf(stderr, "failed to turn off led\n");
+    }
+  }
 
   hid_close(handle);
   hid_exit();
